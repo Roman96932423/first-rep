60adb5416e6f69fbfff3a44d5fc490d294fb9f68
Roman96932423 <ddent358@gmail.com>
2024-09-18 11:19:40 +0200

    creare calculator

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-for i in range(5):
-    print(i)
+# Calculator
+
+user_first_num = int(input("Enter first number: "))
+user_second_num = int(input("Enter second number: "))
+user_action = input("Enter action (+, -, *, /): ")
+
+if user_action == '+':
+    result = user_first_num + user_second_num
+
+if user_action == '-':
+    result = user_first_num - user_second_num
+
+if user_action == '*':
+    result = user_first_num * user_second_num
+
+if user_action == '/':
+    result = user_first_num / user_second_num
+
+print(f'Result {user_first_num} {user_action} {user_second_num}: {result}')
